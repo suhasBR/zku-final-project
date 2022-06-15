9556e46ff9f467c47dae3f4c249f3c8fdcc1a150
suhasbr <suhas.bangalore16@gmail.com>
2022-06-15 21:26:08 +0530

    altered folder structure, added tests for smart contracts

--- circuits/scripts/compile-auth.sh
+++ circuits/scripts/compile-auth.sh
@@ -18,13 +18,17 @@ echo "Compiling auth.circom..."
 circom auth.circom --r1cs --wasm --sym -o auth
 snarkjs r1cs info auth/auth.r1cs
 
-# Start a new zkey and make a contribution
-
+# Start a new zkey 
 snarkjs groth16 setup auth/auth.r1cs powersOfTau28_hez_final_10.ptau auth/circuit_0000.zkey
+# contribute to phase 2 of the ceremony
 snarkjs zkey contribute auth/circuit_0000.zkey auth/circuit_final.zkey --name="1st Contributor Name" -v -e="random text"
+# export the verification key
 snarkjs zkey export verificationkey auth/circuit_final.zkey auth/verification_key.json
 
 # generate solidity contract
-snarkjs zkey export solidityverifier auth/circuit_final.zkey ../contracts/authVerifier.sol
+snarkjs zkey export solidityverifier auth/circuit_final.zkey contracts/authVerifier.sol
+
+# bump solidity version
+sed -i 's/0.6.11;/0.8.4;/g' contracts/authVerifier.sol
 
 cd ..
--- contracts/test/test.js
+++ contracts/test/test.js
@@ -1,1 +1,63 @@
-//Write tests here
+const {expect} = require("chai");
+const {ethers} = require("hardhat");
+const {exportCallDataGroth16} = require("./utils/utils");
+
+describe("authtest", function(){
+    let AuthVerifier, authVerifier;
+
+    before(async function(){
+        AuthVerifier = await ethers.getContractFactory("Verifier");
+        authVerifier = await AuthVerifier.deploy();
+        await authVerifier.deployed();
+    });
+
+    it("should pass auth test", async function(){
+        const pubHash = '19620391833206800292073497099357851348339828238212863168390691880932172496143';
+        const secret = '123';
+        const rString = '456';
+
+        const input = {pubHash, secret, rString};
+
+        let data = await exportCallDataGroth16(
+            input,
+            "./zkp/auth.wasm",
+            "./zkp/circuit_final.zkey"
+        );
+
+        let result = await authVerifier.verifyProof(
+            data.a,
+            data.b,
+            data.c,
+            data.Input
+        );
+
+        expect(result).to.equal(true);
+    });
+
+    it("should fail auth test", async function(){
+        const pubHash = '123';
+        const secret = '123';
+        const rString = '456';
+
+        const input = {pubHash, secret, rString};
+
+        let data = await exportCallDataGroth16(
+            input,
+            "./zkp/auth.wasm",
+            "./zkp/circuit_final.zkey"
+        );
+
+        let result = await authVerifier.verifyProof(
+            data.a,
+            data.b,
+            data.c,
+            data.Input
+        );
+
+        expect(result).to.equal(false);
+    });
+
+    
+
+
+})
