1b24edb2a879fc7c5e48e0f20213b841193979a7
suhasbr <suhas.bangalore16@gmail.com>
2022-06-28 02:04:08 +0530

    vercel check

--- frontend/pages/api/email.js
+++ frontend/pages/api/email.js
@@ -84,6 +84,6 @@ const store_hash = async (addr, hashed_string) => {
 
   const myContract_write = new ethers.Contract(address, myAbi.abi, signer);
   myContract_write.commitHash(addr, hashed_string).then((result) => {
-    
+    console.log(result);
   });
 };
